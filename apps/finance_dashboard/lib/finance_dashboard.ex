38ca7c4947df817cdd076e8abdbf0778b8c918ef
marth141 <ncasados@live.com>
2021-07-03 21:16:26 -0600

    Added functions for evaluating bill dates

--- apps/finance_dashboard/lib/finance_dashboard.ex
+++ apps/finance_dashboard/lib/finance_dashboard.ex
@@ -6,4 +6,19 @@ defmodule FinanceDashboard do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  def bills_within_next_two_weeks_from_pay_date?(bills, pay_date) do
+    bills
+    |> Enum.map(fn bill ->
+      difference = Date.diff(bill.date, pay_date)
+      {:ok, bill, difference >= 0 and difference <= 14}
+    end)
+  end
+
+  def bills_after_next_two_weeks_from_pay_date?(bills, pay_date) do
+    bills
+    |> Enum.map(fn bill ->
+      difference = Date.diff(bill.date, pay_date)
+      {:ok, bill, difference <= 24 and difference > 14}
+    end)
+  end
 end
